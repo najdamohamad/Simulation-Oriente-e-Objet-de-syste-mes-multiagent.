b5753ad0b84ce9b52ac5075a3c9f202eb7a2eee1
Corentin TEISSIER <corentin.teissier@grenoble-inp.org>
2021-11-19 01:25:04 +0100

    netoyage fichier docs, modification Makefile pour la doc

--- codeEtudiants/docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"simulateurBalles","l":"Balls"},{"p":"simulateurBalles","l":"BallsSimulator"},{"p":"simulateurBalles","l":"TestBalls"},{"p":"simulateurBalles","l":"TestBallsSimulator"}];updateSearchResults();
